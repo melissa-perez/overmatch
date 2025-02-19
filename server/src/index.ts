3ca3f06b8059aa2193c7e640832437f48694430c
Melissa Perez <perezmelissa053@gmail.com>
2025-02-19 00:12:06 -0500

    Add rest of starter files

---
+++ server/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
