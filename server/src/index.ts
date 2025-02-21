2950f3ad1eb3457ea5490aea915aa738ff1035bd
Melissa Perez <perezmelissa053@gmail.com>
2025-02-20 23:47:38 -0500

    Incorporate safeguard packages

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,31 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import rateLimit from 'express-rate-limit';
+import auth from '../middleware/authentication';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const ISPRODUCTIONENV = app.get('env') === 'production';
+if (ISPRODUCTIONENV) {
+  app.set('trust proxy', 1);
+  app.use(cors());
+}
+
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  }),
+);
 app.use(express.json());
-app.use(cors());
+app.use(helmet());
+app.use(xss());
 
 app.get('/', (req, res) => {
   res.send('Server is running!');
